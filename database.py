11ed13decbfd45b3d3387989f3bc6d3689b2f09b
Grey <sergejyurskyj@yahoo.com>
2019-10-27 12:23:34 -0700

    Commit 12. Create A Table

--- database.py
+++ database.py
@@ -2,17 +2,27 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
     host='localhost',
-    user='Mysql',
-    passwd='root',
+    user='root',
+    passwd='password123',
+    database='testdb',
 )
 
 # Create Cursor Instance
 my_cursor = mydb.cursor()
 
+# Create A Database
 # my_cursor.execute("CREATE DATABASE testdb")
 
+# Show Database
 my_cursor.execute("SHOW DATABASES")
 
-for db in my_cursor:
-    print(db[0])
+my_cursor.exucute('CREATE TABLE users (name VARCHAR(255), '
+                  'email VARCHAR(255), '
+                  'age INTEGER(10), '
+                  'user_id INTEGER AUTO_INCREMENT PRIMARY KEY)')
+
+my_cursor.execute('SHOW TABLES')
+
+for table in my_cursor:
+    print(table[0])
 
