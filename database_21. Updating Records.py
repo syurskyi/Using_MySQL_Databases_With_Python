22c80134f07886d090e0546321b39b63e164199e
Grey <sergejyurskyj@yahoo.com>
2019-10-27 19:34:49 -0700

    Commit 21. Updating Records

---
+++ database_21. Updating Records.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='password123',
+    database='testdb',
+)
+
+# Create Cursor Instance
+my_cursor = mydb.cursor()
+
+my_sql = "UPDATE users SET age = 40 WHERE name = 'John'"
+
+my_sql = "UPDATE users SET age = 40 WHERE user_id = 6 "
+
+my_cursor.execute(my_sql)
+mydb.commit
